d9bb46dfdfb4106b16806ee7a1c42027c84b14a1
libardoValera <libardo@banderaonline.org>
2025-04-25 16:16:05 -0500

    Ultimos ajustes v.2

--- src/components/CarouselReview.jsx
+++ src/components/CarouselReview.jsx
@@ -10,7 +10,7 @@ import CardReview from "./design/CardReview";
 const CarouselReview = ({ reviews = [], isOwner, info }) => {
   return (
     <div className="w-full md:w-11/12">
-      <Carousel>
+      <Carousel className="relative">
         <CarouselContent>
           {reviews.map((review) => (
             <CarouselItem key={`review_${review.id}`} className="lg:basis-1/2">
@@ -22,7 +22,7 @@ const CarouselReview = ({ reviews = [], isOwner, info }) => {
             </CarouselItem>
           ))}
         </CarouselContent>
-        <div className="hidden lg:flex">
+        <div className="absolute w-[70%] xs:w-[75%] md:w-[70%] xl:w-full mx-auto top-1/2 left-2 right-2 flex items-center justify-between">
           <CarouselPrevious />
           <CarouselNext />
         </div>
