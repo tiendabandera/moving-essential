135c171c54c26623ec557fb286081b894e4872d2
libardoValera <libardo@banderaonline.org>
2025-05-29 13:59:17 -0500

    Ajustes landing realtors

--- src/pages/landing/RealtorPage.jsx
+++ src/pages/landing/RealtorPage.jsx
@@ -80,14 +80,14 @@ const RealtorPage = () => {
     <div className="padding-container w-full flex flex-col gap-24 my-10 md:my-24">
       {/* Section 1 */}
       <section className="flex-center w-full flex-col">
-        <div className="get-app-landing flex-col md:flex-row items-center p-6 sm:px-28 md:px-12">
-          <div className="flex flex-col gap-12">
-            <h2 className="text-center font-bold text-4xl md:text-5xl xl:text-6xl md:text-left xl:max-w-2xl text-pretty leading-tight">
+        <div className="get-app-landing flex-col lg:flex-row items-center p-6 sm:py-20 sm:px-28 md:px-18">
+          <div className="flex flex-col items-center lg:items-start gap-12">
+            <h2 className="text-center font-bold text-4xl md:text-5xl xl:text-6xl lg:text-left xl:max-w-2xl text-pretty leading-tight">
               Connect with Homebuyers{" "}
               <span className="text-white">Who Are Ready to Find You</span>
             </h2>
             <Button
-              className="lg:w-[50%] bg-purple-500 text-color-1 border border-purple-500 rounded-2xl hover:bg-purple-100 hover:text-purple-500"
+              className="md:w-[50%] bg-purple-500 text-color-1 border border-purple-500 rounded-2xl hover:bg-purple-100 hover:text-purple-500"
               onClick={() => navigate("/join/realtors")}
             >
               Create your free profile
@@ -105,19 +105,23 @@ const RealtorPage = () => {
         <img
           src="/assets/img/border.png"
           alt=""
-          className="absolute w-16 md:w-28 right-0 top-0"
+          className="absolute w-16 xl:w-20 right-0 top-0"
         />
         <img
           src="/assets/img/border-2.png"
           alt=""
-          className="absolute w-28 md:w-52 left-0 bottom-0 rotate-180"
+          className="absolute w-28 xl:w-36 left-0 bottom-0 rotate-180"
         />
-
-        <h2 className="text-color-1 text-3xl lg:text-4xl font-bold sm:max-w-2xl lg:max-w-3xl xl:max-w-4xl text-center">
+        <img
+          src="/assets/svg/landing-realtor-5.svg"
+          alt="Feature"
+          className="size-32 sm:size-40"
+        />
+        <h2 className="text-color-1 text-2xl md:text-3xl font-bold sm:max-w-2xl lg:max-w-3xl xl:max-w-4xl text-center">
           Moving Essential is the go-to platform for future homeowners looking
           for a trusted realtor.
         </h2>
-        <h2 className="text-3xl lg:text-4xl font-bold sm:max-w-2xl lg:max-w-3xl xl:max-w-4xl text-center">
+        <h2 className="text-2xl md:text-3xl font-bold sm:max-w-2xl lg:max-w-3xl xl:max-w-4xl text-center">
           List your profile for free and appear in their searches at the exact
           moment they`re ready to take action.
         </h2>
@@ -167,7 +171,7 @@ const RealtorPage = () => {
       </section>
       {/* Section 5 */}
       <section className="max-container flex items-center w-full gap-5">
-        <div className="get-app-landing-2 flex-row p-8 xl:p-10 !items-center justify-center mt-auto lg:items-start h-[470px] max-h-[500px] xl:h-[450px]">
+        <div className="get-app-landing-2 flex-row p-8 xl:p-10 !items-center justify-center mt-auto lg:items-start h-[410px] max-h-[500px] xl:h-[450px]">
           <div className="w-full flex flex-col gap-5">
             <h2 className="bold-32 lg:bold-52 text-color-1">Who Can Join?</h2>
             <ul className="text-white text-lg space-y-4">
@@ -199,7 +203,7 @@ const RealtorPage = () => {
         </div>
         <img
           src="/assets/img/landing-realtor-3.png"
-          className="hidden lg:block w-[50%] lg:h-[520px] xl:w-1/2 mt-auto xl:h-[500px]"
+          className="hidden lg:block w-1/2 mt-auto lg:h-[450px] xl:h-[500px]"
           alt=""
         />
       </section>
@@ -244,7 +248,7 @@ const RealtorPage = () => {
       </section>
       {/* Section 7 */}
       <section className="w-full flex flex-col">
-        <div className="get-app-landing-3 lg:h-[450px] flex-col items-center justify-center p-10 sm:px-20 sm:py-20 md:pb-0 xl:pt-10">
+        <div className="get-app-landing-3 lg:h-[300px] flex-col items-center justify-center p-0">
           <div className="w-full flex flex-col items-center gap-10">
             <h2 className="bold-32 lg:bold-52">Join our Network</h2>
             <Button
